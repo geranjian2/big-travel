6e67eb41e32b35df75e9403ecb3a99e449adf345
Andres Jimenez <andres.jimenez@grupomatrix.co>
2022-03-25 00:17:28 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,18 @@ import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: '', 
+    path: '',
     loadChildren: () => import('./shared/layouts/big-travel/big-travel.module').then(m => m.BigTravelModule),
     canActivate: [AuthGuard]
   },
   {
-    path: 'login', 
+    path: 'login',
     loadChildren: () => import('./shared/layouts/big-travel-auth/big-travel-auth.module').then(m => m.BigTravelAuthModule),
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{ useHash: true })],
+  imports: [RouterModule.forRoot(routes,{ useHash: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
